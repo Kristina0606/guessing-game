5c226e114945b712f5bb04685730f3ff08da9252
kristina <krisbubenka@icloud.com>
2024-09-20 20:04:52 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
+/** @format */
+
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.currentNum = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.currentNum = Math.ceil((this.min + this.max) / 2);
+    console.log(this.currentNum);
+    return this.currentNum;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.currentNum;
 
-    }
+    console.log(this.max);
+  }
 
-    lower() {
+  greater() {
+    this.min = this.currentNum;
 
-    }
-
-    greater() {
-
-    }
+    console.log(this.min);
+  }
 }
-
 module.exports = GuessingGame;
